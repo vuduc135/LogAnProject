e40848b022620f10cb6a9dc9c79d9661bbda2933
duc.vu@gmail.com <duc.vu@niteco.se>
2016-11-24 16:13:30 +0700

    Add Log Analyzer class

---
+++ LogAn/LogAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogAn
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (!fileName.EndsWith(".SLF"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
